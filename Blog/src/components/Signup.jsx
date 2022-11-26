218fe87c89ef6a80295ad79f9e3c8de82868a29c
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-26 17:34:24 +0530

    removed tags from forms

--- Blog/src/components/Signup.jsx
+++ Blog/src/components/Signup.jsx
@@ -10,16 +10,20 @@ const Signup = () => {
                 <div className="d-flex justify-content-between p-5 rounded-3">
                     <div className="form-content" style={{ width: "22em" }}>
                         <div className="mb-3">
-                            <label htmfor="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="rounded-0 form-control mb-3" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <label htmfor="" className="form-label">Name</label>
+                            <input type="text" className="rounded-0 form-control mb-3" id="" />
                         </div>
                         <div className="mb-3">
-                            <label htmfor="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="rounded-0 form-control mb-3" id="exampleInputPassword1" />
+                            <label htmfor="" className="form-label">Email address</label>
+                            <input type="email" className="rounded-0 form-control mb-3" id="" />
                         </div>
                         <div className="mb-3">
-                            <label htmfor="exampleInputPassword1" className="form-label">Re-enter Password</label>
-                            <input type="password" className="rounded-0 form-control mb-3" id="exampleInputPassword1" />
+                            <label htmfor="" className="form-label">Password</label>
+                            <input type="password" className="rounded-0 form-control mb-3" id="" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmfor="" className="form-label">Re-enter Password</label>
+                            <input type="password" className="rounded-0 form-control mb-3" id="" />
                         </div>
                         <button type="submit" className="btn btn-primary rounded-0 border-0 mt-4" style={{ backgroundColor: "#f7a60f" }}>Submit</button>
                         <p className='mt-5'>Already have an account ?<Link to="/Login" style={{ color: "#f7a60f" }}> Login</Link> </p>
