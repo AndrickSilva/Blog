d408005280796c1ac7995251749d98c46f434300
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-28 10:56:41 +0530

    pasted files outside

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsInstagram, BsTwitter, BsLinkedin, BsFacebook } from 'react-icons/bs'
 
 const Footer = () => {
     return (
-        <div className='bg-black d-flex justify-content-around align-items-center py-3'>
+        <div className='bg-black d-flex justify-content-around align-items-center py-3 '>
             {/* <p className='text-white'>&#169; 2001 - 2022 Blogger | All Rights Reserved</p> */}
             <div className=' fs-4 d-flex gap-4'>
                 <a className='text-white' href="#"> <BsInstagram /></a>
